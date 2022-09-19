330167a1272eb3f1702d6fc7bedbc04826df3d20
Ragini <113632912+Raginim2728@users.noreply.github.com>
2022-09-20 00:20:26 +0530

    Add files via upload

---
+++ mathoperations/Subtraction.java
@@ -0,0 +1,15 @@
+package mathoperations;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		int num1, num2, ans;
+		num1=94;
+		num2=65;
+		ans=num1-num2;
+		System.out.println("sum="+ans);
+		System.out.println("The sum of "+num1+" and "+num2+" is "+ans);
+
+	}
+
+}

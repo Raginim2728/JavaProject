ec3015ddc44d4ba00216b779daf60722395c1954
Ragini <113632912+Raginim2728@users.noreply.github.com>
2022-10-08 10:09:31 +0530

    Add files via upload

---
+++ MainEx.java
@@ -0,0 +1,29 @@
+package todaypgm;
+abstract class A
+{
+	abstract void m1();
+}
+abstract class B extends A
+{
+	void m2()
+	{
+		System.out.println("m2 method");
+	}
+}
+class C extends B
+{
+	@Override
+	void m1()
+	{
+		System.out.println("m1 method");
+	}
+}
+public class MainEx {
+
+	public static void main(String[] args) {
+		C ob=new C();
+		ob.m1();
+		ob.m2();
+	}
+
+}
